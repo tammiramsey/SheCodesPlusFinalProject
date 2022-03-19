6336a5a1fad8cecacb844e5fad286d06a8b9706d
tammiramsey <tammirice@gmail.com>
2022-03-18 21:41:18 -0400

    working on adding current date and time to weather app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,49 @@
-    
+// current date & time
+function formatDate() {
+  let now = new Date();
+
+  let date = now.getDate();
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let year = now.getFullYear();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursady",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+
+  let today = `${day}, ${month} ${date}, ${year} ${hours}:${minutes}`;
+  return today;
+}
+
+let today = document.querySelector(#current-date-time);
+
+
